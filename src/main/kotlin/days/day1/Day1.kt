512e2aecda079e0df7a79cdc89a70358546d8eba
Cruz Ogden <tcogden23@gmail.com>
2020-12-02 12:56:35 -0700

    updates day1 to handle 1010 single case

--- src/main/kotlin/days/day1/Day1.kt
+++ src/main/kotlin/days/day1/Day1.kt
@@ -9,7 +9,6 @@ class Day1 : () -> Int? {
     override fun invoke(): Int? {
         val lines = reader.read(inputPath).map { it.toInt() }
         val result = getMultipleOf2020Sum(lines)
-        println(result)
         return if (result != null) result.first * result.second else null
     }
 
@@ -31,9 +30,20 @@ class Day1 : () -> Int? {
     }
 
     private fun matchSum(numbers: List<Int>, match: Int): Pair<Int, Int>? {
-        for (number in numbers) {
-            val result = numbers.binarySearch(match - number)
-            return if (result >= 0) number to numbers[result] else continue
+        val matchIsEven = match % 2 == 0
+        for (index in numbers.indices) {
+            val number = numbers[index]
+            if (matchIsEven && number == match / 2) {
+                val nextNumber = if (index + 1 < numbers.size) numbers[index + 1] else null
+                if (nextNumber == number) {
+                    return number to nextNumber
+                } else {
+                    continue
+                }
+            } else {
+                val result = numbers.binarySearch(match - number)
+                return if (result >= 0) number to numbers[result] else continue
+            }
         }
 
         return null
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,10 @@
+import days.day1.Day1
 import days.day2.Day2
-import days.day2.Day2Part2
 
 fun main() {
+    val day1 = Day1()
+    val day1Result = day1()
+    println(day1Result)
     val day2 = Day2()
     val day2Result = day2()
     println(day2Result)

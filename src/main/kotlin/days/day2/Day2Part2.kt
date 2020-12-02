716bab53c2fa11e22f8ba607b83087b01ad20dbe
Cruz Ogden <tcogden23@gmail.com>
2020-12-02 01:34:14 -0700

    Adds Day 2 Part 2

---
+++ src/main/kotlin/days/day2/Day2Part2.kt
@@ -0,0 +1,26 @@
+package days.day2
+
+import util.FileReader
+
+class Day2Part2 : () -> Int {
+    private val reader = FileReader()
+    private val inputPath = "src/main/resources/day2/day2.txt"
+
+    override fun invoke(): Int {
+        val lines = reader.read(inputPath)
+        return lines.sumBy { line ->
+            val (min, max, requiredValue, password) = Day2PasswordPayload.fromLine(line)
+
+            val passwordChars = password.toCharArray()
+            val firstValue = passwordChars[min - 1] == requiredValue
+            val secondValue = passwordChars[max - 1] == requiredValue
+
+            if (firstValue && !secondValue || !firstValue && secondValue) {
+                1
+            } else {
+                0
+            }
+        }
+    }
+
+}

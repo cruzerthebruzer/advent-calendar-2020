4a321df781ddea9c653f51e44dc63edba17560b5
Cruz Ogden <tcogden23@gmail.com>
2020-12-07 06:35:42 -0700

    adds day 6 deps

---
+++ src/main/kotlin/days/day6/Day6.kt
@@ -0,0 +1,27 @@
+package days.day6
+
+import util.FileReader
+import util.LineAggregator
+
+class Day6 : (List<String>) -> Int {
+    override fun invoke(lines: List<String>): Int = countQuestions(lines)
+}
+
+object Day6Runner {
+    private val reader = FileReader()
+    private val inputPath = "src/main/resources/day6/day6.txt"
+    private val day6 = Day6()
+    private val lineAggregator = LineAggregator()
+
+    fun run(): Int {
+        val lines = reader.read(inputPath)
+        return day6(lineAggregator.aggregate(lines, lineSeparator = ""))
+    }
+}
+
+fun countQuestions(lines: List<String>) = lines.sumBy { line ->
+    val set = mutableSetOf<Int>()
+    line.chars().forEach { char -> set.add(char) }
+    set.size
+
+}
